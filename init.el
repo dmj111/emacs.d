6393487fb6f74675252a6c2b3f0e947da0827ed1
Dave Jones <dmj111@gmail.com>
2017-04-30 13:05:23 -0400

    Setup to use conda better.

--- init.el
+++ init.el
@@ -62,6 +62,9 @@
 
 (defconst *is-mac* (eq system-type 'darwin))
 
+(defvar *anaconda-directory* "/Users/dave/anaconda" 
+  "Anaconda installation directory")
+
 ;; Add a local lisp directory to the load path.
 (add-to-list 'load-path *local-dir*)
 
@@ -704,6 +707,26 @@ file of a buffer in an external program."
   :commands (wttrin)
   :init
   (setq wttrin-default-cities '("State College, Pennsylvania")))
+
+
+(use-package conda
+  :ensure t
+  :config
+  ;; Make sure pylint is installed.
+  ;; make sure a default environment is set
+  ;;   conda env export > environment.yml
+  ;; https://github.com/necaris/conda.el
+  ;; if you want interactive shell support, include:
+  (conda-env-initialize-interactive-shells)
+  ;; if you want eshell support, include:
+  (conda-env-initialize-eshell)
+  ;; if you want auto-activation (see below for details), include:
+  (conda-env-autoactivate-mode t)
+  ;; If your Anaconda installation is anywhere other than the default (~/.anaconda3) then set the conda-anaconda-home custom variable to the installation path. For instance, in my configuration I have:
+
+  (custom-set-variables
+   '(conda-anaconda-home *anaconda-directory*)))
+
 
 ;; Load the local file, if it exists.
 (require 'init-local nil t)
