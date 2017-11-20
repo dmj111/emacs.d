e7a37ea3193c68f1487b5e29f2c8022028bf58f5
Dave Jones <dmj111@gmail.com>
2017-11-19 22:05:28 -0500

    update config for melpa.

--- init.el
+++ init.el
@@ -139,9 +139,9 @@
              '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("org" . "http://orgmode.org/elpa/") t)
-(when *is-mac*
-  (add-to-list 'package-archives
-               '("marmalade" . "http://marmalade-repo.org/packages/") t))
+;; (when *is-mac*
+  ;; (add-to-list 'package-archives
+  ;;              '("marmalade" . "http://marmalade-repo.org/packages/") t))
 (require 'package)
 (add-to-list 'package-archives
          '("melpa" . "http://melpa.org/packages/") t)
