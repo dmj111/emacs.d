e2e7b36b55b1af3887e3575b73beeb4aefd6f59a
Dave Jones <dmj111@gmail.com>
2018-02-01 21:52:28 -0500

    remove orgmode elpa

--- init.el
+++ init.el
@@ -137,8 +137,8 @@
 
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("org" . "http://orgmode.org/elpa/") t)
+;; (add-to-list 'package-archives
+;;              '("org" . "http://orgmode.org/elpa/") t)
 ;; (when *is-mac*
   ;; (add-to-list 'package-archives
   ;;              '("marmalade" . "http://marmalade-repo.org/packages/") t))
